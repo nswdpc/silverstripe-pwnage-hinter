84abb259b43c7e302dc6a4f3ed65dadec9c2498f
JamesDPC <69664712+JamesDPC@users.noreply.github.com>
2026-08-04 04:16:32 +0000

    [rector] Automated updates generated by rector configuration

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NSWDPC\Pwnage;
 
 class ApiException extends \Exception
--- src/Exceptions/PwnedPasswordException.php
+++ src/Exceptions/PwnedPasswordException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NSWDPC\Pwnage;
 
 class PwnedPasswordException extends \Exception
